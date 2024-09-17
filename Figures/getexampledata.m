42267b842885d9c1dd291e4f6d7ca628d02edd6d
Francisco J. Flores <fjflores@mit.edu>
2024-09-17 12:42:12 -0400

    succesfull refactoring to deal with all doses and sleep example.

--- Figures/getexampledata.m
+++ Figures/getexampledata.m
@@ -21,22 +21,22 @@ f2load = "example_traces.csv";
 tsTab = readtable( fullfile( resDir, f2load ) );
 
 %% load dex experiment
-dexIdx = strcmp( tsTab.expType, 'dex' );
-expId = tsTab.expId( dexIdx );
-nExps = length( expId );
+% allExpIdx = strcmp( tsTab.expType, 'dex' );
+% expId = tsTab.expId( allExpIdx );
+nExps = height( tsTab );
 for expIdx = 1 : nExps
-    thisExp = expId( expIdx );
+    thisExp = tsTab.expId( expIdx );
     fprintf( 'Loading exp %u...', thisExp )
     t1 = tic;
     ephysData = loadprocdata( thisExp );
     t2 = toc( t1 );
-    fprintf( 'done in %s\n', humantime( t2 ) )
+    fprintf( 'done in %s.\n', humantime( t2 ) )
 
     tSpec = ephysData.spec.t;
     f = ephysData.spec.f;
 
-    tSpec1 = tsTab.tsSpec1( dexIdx );
-    tSpec2 = tsTab.tsSpec2( dexIdx );
+    tSpec1 = tsTab.tsSpec1( tsTab.expId == thisExp );
+    tSpec2 = tsTab.tsSpec2( tsTab.expId == thisExp );
     idxSpec = tSpec >= tSpec1 & tSpec <= tSpec2;
     fIdx = f <= 40;
     f2plot = f( fIdx );
@@ -52,55 +52,36 @@ for expIdx = 1 : nExps
     emg2plot = ephysData.emg.smooth( idxEmg );
 
     ts = ephysData.eeg.ts( :, 1 );
-    tBase1 = tsTab.tsBase1( dexIdx );
+    tBase1 = tsTab.tsBase1( tsTab.expId == thisExp );
     idxBase = getepochidx( ts, tBase1, 10 );
     eegBase = ephysData.eeg.filt( idxBase, : );
     tsBase = ts( idxBase );
 
-    tDex1 = tsTab.tsDex1( dexIdx );
-    idxDex = getepochidx( ts, tDex1, 10 );
-    eegDex = ephysData.eeg.filt( idxDex, : );
-    tsDex = ts( idxDex );
+    tExp1 = tsTab.tsExp1( tsTab.expId == thisExp );
+    idxExp = getepochidx( ts, tExp1, 10 );
+    eegExp = ephysData.eeg.filt( idxExp, : );
+    tsExp = ts( idxExp );
 
-    %% Load sleep experiment
-    if expIdx == nExps
-        clear ephysData
-        sleepIdx = strcmp( tsTab.expType, 'sleep' );
-        expId = tsTab.expId( sleepIdx );
-        fprintf( 'Loading exp %u...', thisExp )
-        t1 = tic;
-        ephysData = loadprocdata( expId );
-        t2 = toc( t1 );
-        fprintf( 'done in %s\n', humantime( t2 ) )
-
-        tSleep1 = tsTab.tsBase1( sleepIdx );
-        idxSleep = ts >= tSleep1 & ts <= tSleep1 + 10;
-        eegSleep = ephysData.eeg.filt( idxSleep, : );
-        tsSleep = ts( idxSleep );
-    end
-
-    %% Pad eeg and ts vectors
-    [ eegBaseL, eegSleepL, eegDexL ] = padvectors(...
-        eegBase( :, 1 ), eegSleep( :, 1 ), eegDex( :, 1 ),...
-        "nans" );
-    [ eegBaseR, eegSleepR, eegDexR ] = padvectors(...
-        eegBase( :, 2 ), eegSleep( :, 2 ), eegDex( :, 2 ),...
-        "nans" );
-    [ tsBase1, tsSleep1, tsDex1 ] = padvectors( tsBase, tsSleep, tsDex,...
-        "linear" );
+    % Pad eeg and ts vectors
+    [ eegBaseL, eegExpL ] = padvectors(...
+        eegBase( :, 1 ), eegExp( :, 1 ), "nans" );
+    [ eegBaseR, eegExpR ] = padvectors(...
+        eegBase( :, 2 ), eegExp( :, 2 ), "nans" );
+    [ tsBase1, tsExp1 ] = padvectors( tsBase, tsExp, "linear" );
 
     %% Save data for plotting to figures folder
     if saveFlag
         fprintf( "Saving figure data..." )
+        info( expIdx ).expId = thisExp;
+        info( expIdx ).dose = tsTab.dose( tsTab.expId == thisExp );
+        info( expIdx ).type = tsTab.expType{ tsTab.expId == thisExp };
+
         eeg( expIdx ).base.L = eegBaseL;
         eeg( expIdx ).base.R = eegBaseR;
-        eeg( expIdx ).dex.L = eegDexL;
-        eeg( expIdx ).dex.R = eegDexR;
-        eeg( expIdx ).sleep.L = eegSleepL;
-        eeg( expIdx ).sleep.R = eegSleepR;
+        eeg( expIdx ).exp.L = eegExpL;
+        eeg( expIdx ).exp.R = eegExpR;
         eeg( expIdx ).t2plot.base = tsBase1;
-        eeg( expIdx ).t2plot.dex = tsDex1;
-        eeg( expIdx ).t2plot.sleep = tsSleep1;
+        eeg( expIdx ).t2plot.exp = tsExp1;
 
         emg( expIdx ).smooth = emg2plot;
         emg( expIdx ).t2plot = tEmg2plot;
@@ -111,7 +92,7 @@ for expIdx = 1 : nExps
         spec( expIdx ).f2plot = f2plot;
 
         f2save = "ExampleFigData.mat";
-        save( fullfile( resDir, f2save ), "eeg", "emg", "spec" )
+        save( fullfile( resDir, f2save ), "info", "eeg", "emg", "spec" )
 
         fprintf( "Done!\n" )
 
