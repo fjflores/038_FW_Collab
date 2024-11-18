0318d20bc34c686a447c46db2cbb0db6ad8f9544
Francisco J. Flores <fjflores@mit.edu>
2024-11-18 11:51:39 -0500

    make a fully working gettidy data function.

--- gettidydata.m
+++ gettidydata.m
@@ -1,4 +1,4 @@
-function getexampledata( mouseId, csvFile, tLims, saveFlag )
+function gettidydata( mouseId, csvFile, tLims, saveFlag )
 % GETEXAMPLEDATA picks data from full experiments and saves it.
 %
 % Usage:
@@ -6,25 +6,23 @@ function getexampledata( mouseId, csvFile, tLims, saveFlag )
 %
 % Input:
 % mouseId: mouse ID.
-% maxFreq: maximu frequency to extrcat for spectrogram.
 % csvFile: name of csvFile to use.
 % tLims: epoch to extract around time of injection.
-% saveFlag: boolean to flag whether to save the figure data. Ddefault:
-% true.
+% saveFlag: boolean to flag whether to save the figure data. Default: true.
 
 % Set defaults
-if ~exist( "saveFlag", "var" )
-    saveFlag = true;
+if ~exist( "csvFile", "var" )
+    csvFile = "abc_experiment_list.xlsm";
 
 end
 
-if isempty( tLims )
+if ~exist( "tLims", "var" )
     tLims = [ 300 3600 ];
 
 end
 
-if isempty( csvFile )
-    csvFile = "abc_experiment_list.xlsm";
+if ~exist( "saveFlag", "var" )
+    saveFlag = true;
 
 end
 
@@ -33,94 +31,102 @@ resDir = fullfile( root, "Results" );
 masterTab = readtable( fullfile( resDir, csvFile ) );
 
 % get experiments to load
-doseSortTab = sortrows( masterTab, "dex_dose_ugperkg" );
-exps2procIdx = doseSortTab.analyze == 1 & doseSortTab.mouse_id == mouseId;
+doseSortTab = sortrows( masterTab, "drug_dose" );
+exps2procIdx = ...
+    doseSortTab.analyze == 1 & ...
+    doseSortTab.mouse_id == mouseId & ...
+    doseSortTab.drug == "dex";
 exps2proc = doseSortTab.exp_id( exps2procIdx );
-doses = doseSortTab.dex_dose_ugperkg( exps2procIdx );
+doses = doseSortTab.drug_dose( exps2procIdx );
 
 % load dex experiment
+t1 = tic;
 nExps = length( exps2proc );
 for expIdx = 1 : nExps
     thisExp = exps2proc( expIdx );
     thisExpIdx = masterTab.exp_id == thisExp;
     fprintf( 'Loading %s exp %u...', mouseId, thisExp )
-    t1 = tic;
-    ephysData = loadprocdata( thisExp );
-    t2 = toc( t1 );
-    fprintf( 'done in %s.\n', humantime( t2 ) )
-    
+    [ eegRaw, emgRaw ] = loadprocdata( thisExp, { "eegClean", "emgRaw" } );
+    fprintf( 'done.\n' )
+
     tInj1 = masterTab.dex_ts_inj( thisExpIdx ) - tLims( 1 ); % epoch before
     tInj2 = masterTab.dex_ts_inj( thisExpIdx ) + tLims( 2 ); % epoch after
-    tEmg = ephysData.emg.tRaw;
-    idxEmg = tEmg >= tInj1 & tEmg <= tInj2;
-    tEmg2plot = tEmg( idxEmg );
-    emg2proc = ephysData.emg.filt( idxEmg );
 
-    % filter and downsample emg.
-    
+    % Downsample adn filter emg
+    fprintf( " Processing emg..." )
+    decFactor = 3;
+    chunkIdx = emgRaw.ts >= tInj1 & emgRaw.ts <= tInj2;
+    tEmg = decimate( emgRaw.ts( chunkIdx ), decFactor );
+    emgTmp = eegemgfilt( emgRaw.data, [ 10 900 ], emgRaw.Fs );
+    emgTmp( ~chunkIdx ) = [ ];
+    emgFilt = decimate( emgTmp, decFactor );
+    fprintf( "done.\n" )
 
+    % Downsample and filter eeg
+    fprintf( " Processing eeg..." )
+    decFactor = 10;
+    tEeg = decimate( emgRaw.ts( chunkIdx ), decFactor );
+    eegTmp = eegemgfilt( eegRaw.data, [ 0.5 100 ], eegRaw.Fs );
 
-    % Get all EEG.
-    % Spec for plotting
-    ts = ephysData.eeg.ts( :, 1 );
-    tAllIdx = ts >= tInj1 & ts <= tInj2;
-    tsAll = ts( tAllIdx );
-    eegAll = ephysData.eeg.clean( tAllIdx, : );
-    
     % isolate baseline and compute z-score
     tOff = masterTab.dex_ts_offline( thisExpIdx );
     tOn = masterTab.dex_ts_online( thisExpIdx );
-    tBaseZIdx = ts <= tOff;
-    params = struct(...
-        'tapers', [ 3 5 ],...
-        'Fs', ephysData.eeg.Fs( 1 ),...
-        'fpass', [ 0.5 40 ],...
-        'pad', 1 );
-    win = [ 15 1.5 ];
-    eegZAll = zeros( size( eegAll ) );
+
+    % eegZ = zeros( size( eegFilt ) );
     for eegIdx = 1 : 2
-        mu = mean( ephysData.eeg.clean( tBaseZIdx, eegIdx ) );
-        sigma = std( ephysData.eeg.clean( tBaseZIdx, eegIdx ) );
+        eegFilt = decimate( eegTmp( chunkIdx, eegIdx ), decFactor );
+        tBaseZIdx = tEmg <= tOff;
+        mu = mean( eegFilt( tBaseZIdx ) );
+        sigma = std( eegFilt( tBaseZIdx ) );
         % sprintf( "EEG: %u, Size(eegAll) %u x %u, Size(eegZAll) %u x %u \n",...
         %     eegIdx, )
-        eegZAll( :, eegIdx) = ( eegAll( :, eegIdx ) - mu ) ./ sigma;
+        eegZ( :, eegIdx) = ( eegFilt - mu ) ./ sigma;
 
     end
+    fprintf( "done.\n" )
 
     % Get new spectrogram
-    [ S, t, f ] = mtspecgramc( eegZAll, win, params );
+    fprintf( " Processing spec..." )
+    params = struct(...
+        'tapers', [ 3 5 ],...
+        'Fs', eegRaw.Fs( 1 ),...
+        'fpass', [ 0.5 100 ],...
+        'pad', 1 );
+    win = [ 15 1.5 ];
+    [ S, t, f ] = mtspecgramc( eegZ, win, params );
+    fprintf( "done.\n" )
 
-    %% Save data for plotting to figures folder
-    if saveFlag
-        fprintf( "Saving figure data..." )
-        info( expIdx ).expId = thisExp;
-        info( expIdx ).dose = doses( expIdx );
-        info( expIdx ).type = masterTab.drug{ thisExpIdx };
-        info( expIdx ).injDex = masterTab.dex_ts_inj( thisExpIdx );
-        info( expIdx ).injOff = tOff;
-        info( expIdx ).injOn = tOn;
-        
-        eeg( expIdx ).all.L = eegAll( :, 1 );
-        eeg( expIdx ).all.R = eegAll( :, 2 );
-        eeg( expIdx ).all.t2plot = tsAll;
-        eeg( expIdx ).all.ZL = eegZAll;
+    info( expIdx ).expId = thisExp;
+    info( expIdx ).dose = doses( expIdx );
+    info( expIdx ).type = masterTab.drug{ thisExpIdx };
+    info( expIdx ).injDex = masterTab.dex_ts_inj( thisExpIdx );
+    info( expIdx ).injOff = tOff;
+    info( expIdx ).injOn = tOn;
 
-        emg( expIdx ).filt = emg2plot;
-        emg( expIdx ).t2plot = tEmg2plot;
+    eeg( expIdx ).L = eegZ( :, 1 );
+    eeg( expIdx ).R = eegZ( :, 2 );
+    eeg( expIdx ).t = tEeg;
 
-        spec( expIdx ).L = squeeze( S( :, :, 1 ) );
-        spec( expIdx ).R = squeeze( S( :, :, 2 ) );
-        spec( expIdx ).t2plot = t;
-        spec( expIdx ).f2plot = f;
+    emg( expIdx ).filt = emgFilt;
+    emg( expIdx ).t = tEmg;
 
-        f2save = "ExampleFigData.mat";
-        save( fullfile( resDir, mouseId, f2save ), ...
-            "info", "eeg", "spec", "emg", "-v7.3" )
-        fprintf( "Done!\n" )
-
-    end
+    spec( expIdx ).L = squeeze( S( :, :, 1 ) );
+    spec( expIdx ).R = squeeze( S( :, :, 2 ) );
+    spec( expIdx ).t = t;
+    spec( expIdx ).f = f;
 
 end
-disp( 'Done processing everything.' )
+
+%% Save data for plotting to figures folder
+if saveFlag
+    fprintf( " Saving tidy data..." )
+    f2save = "TidyData.mat";
+    save( fullfile( resDir, mouseId, f2save ), ...
+        "info", "eeg", "spec", "emg", "-v7.3" )
+    fprintf( "done.\n\n" )
+
+end
+
+fprintf( 'Done processing %s in %s.\n', mouseId, toc( t1 ) )
 
 
