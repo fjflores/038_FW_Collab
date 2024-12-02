9a0c649059bd9e62653ba828f4c6a80905fa12f6
Francisco J. Flores <fjflores@mit.edu>
2024-12-02 14:44:51 -0500

    uses time from each iteration to throw total time.

--- DoseEffect/batchtidydata.m
+++ DoseEffect/batchtidydata.m
@@ -1,8 +1,13 @@
 function batchtidydata( miceList, csvFile, tLims, saveFlag )
 
+totTime = 0;
 nMice = length( miceList );
 for mIdx = 1 : nMice
     mouseId = miceList{ mIdx };
-    gettidydata( mouseId, csvFile, tLims, saveFlag )
+    eTime = gettidydata( mouseId, csvFile, tLims, saveFlag );
+    totTime = totTime + eTime;
 
 end
+fprintf( "Total processing took %s\n", humantime( totTime ) )
+
+
