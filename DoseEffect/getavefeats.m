c008dd23d67e3f67d4ee31e36bc0be9d5ea9fc4c
Francisco J. Flores <fjflores@mit.edu>
2024-12-02 19:04:11 -0500

    rename and move plotqeeg.

---
+++ DoseEffect/getavefeats.m
@@ -0,0 +1,138 @@
+function featTab = plotqeeg( doses, kind, tLims )
+% PLOTQEEG plot a qeeg feature across all mice for the given doses.
+% 
+% Usage:
+% qeeg = plotqeeg( doses, kind )
+% 
+% Input:
+% doses: vector with dose or doses to be plotted and extracted.
+% kind: string whith qeeg feature to plot/extract. "mf" for median freq, 
+% "sef" for spectral edge, and "df" for dominant frequency.
+% tLims: time limits to take qeeg median.
+% 
+% Output:
+% qeeg: requested spectral feature
+
+if nargin < 3
+    tLims = [ 30 40 ];
+
+end
+
+root = getrootdir( );
+csvFileMaster = "abc_experiment_list.xlsm";
+masterTab = readtable( fullfile( root, "Results", csvFileMaster ) );
+
+featTab = table(...
+    'Size', [ 5, 5 ], ...
+    'VariableTypes', { 'double', 'string', 'double', 'double', 'double' }, ...
+    'VariableNames', {'expId', 'mouseId', 'dose', 'sef', 'Pdelta' } );
+
+% gap = [ 0.005 0.01 ];
+% margH = [ 0.1 0.05 ];
+% margV = [0.1 0.1];
+% opts = { gap, margH, margV };
+% yLims = [ 0 50 ];
+
+% colormap magma
+
+% tits = dictionary(...
+%     "mf", "Median Fequency", ...
+%     "sef", "Spectral Edge", ...
+%     "df", "Dominant Frequency" );
+
+nDoses = length( doses );
+cnt = 1;
+for doseIdx = 1 : nDoses
+    thisDose = doses( doseIdx );
+    fprintf( "Processing dose %u %cg\\kg...\n", thisDose, 965 )
+    expListIdx = masterTab.analyze == 1 & ...
+        masterTab.drug == "dex" & ...
+        masterTab.drug_dose == thisDose;
+    expList = masterTab.exp_id( expListIdx );
+    nExps = length( expList );
+
+    % hAx( doseIdx ) = subtightplot( nDoses, 1, doseIdx, opts{ : } );
+    for expIdx = 1 : nExps
+        thisExp = expList( expIdx );
+        metDat = getmetadata( thisExp );
+        resDir = fullfile( root, "Results", metDat.subject );
+        f2load = "TidyData.mat";
+        load( fullfile( resDir, f2load ), "spec", "info" );
+        tabExpIdx = find( [ info.expId ] == thisExp );
+        S = spec( tabExpIdx ).SL;
+        t = ( spec( tabExpIdx ).t - info( tabExpIdx ).injDex ) / 60;
+        f = spec( tabExpIdx ).f;
+
+        % Get spectra after injection
+        dexIdxS = t > 0;
+        Sdex = S( dexIdxS, : );
+        t2plot = t( dexIdxS );
+
+        [ mf, sef, df ] = qeegspecgram( Sdex, f, [ 0.5 30 ] );
+        P = powerperband( Sdex, f, [ 0.5 5 ], 'total' );
+
+        switch kind
+            case "mf"
+                var2plot = mf;
+
+            case "sef"
+                var2plot = sef;
+
+            case "df"
+                var2plot = df;
+
+        end
+        
+
+        featTab.expId( cnt ) = thisExp;
+        featTab.mouseId( cnt ) = string( metDat.subject );
+        featTab.dose( cnt ) = thisDose;
+        featTab.sef( cnt ) = median( var2plot(...
+            t2plot > tLims( 1 ) & t2plot < tLims( 2 ) ) );
+        featTab.Pdelta( cnt ) = sum(...
+            P( t2plot > tLims( 1 ) & t2plot < tLims( 2 ) ) );
+        % plot( t2plot, var2plot, Color=[ 0.5 0.5 0.5 ] )
+        % box off
+        % hold on
+        cnt = cnt + 1;
+        disprog( expIdx, nExps, 20 )
+
+    end
+
+    % ylabel( 'Freq. (Hz)' )
+    % xLims = get( gca, 'xlim' );
+    % yLims = get( gca, 'ylim' );
+    % posX = xLims( 1 ) + 1;
+    % posY = yLims( 1 ) + 4;
+    % 
+    % if thisDose == 0
+    %     leg = "Saline";
+    %     title( tits( kind ) )
+    % 
+    % else
+    %     leg = sprintf( "%u %cg/kg", thisDose, 956 );
+    % 
+    % end
+    % text( posX, posY, leg,...
+    %     'Color', 'k',...
+    %     'FontWeight', 'bold',...
+    %     'FontSize', 10 )
+    % ylabel( 'Freq. (Hz)' )
+
+end
+
+% Remove zeros from qEEG.
+% qeeg( qeeg == 0 ) = NaN;
+% 
+% 
+% set( hAx,...
+%     'FontSize', 12,...
+%     'TickDir', 'out',...
+%     'XTickLabel', [],...
+%     'YTick',  0 : 10 : 40  )
+% 
+% set( hAx( end ),...
+%     "XTick", [ -10 : 10 : 60 ],...
+%     "XTickLabel", [ -10 : 10 : 60 ] )
+% xlabel( hAx( 1 ), "time (min)" );
+% set( hAx, 'FontSize', 12, 'TickDir', 'out' )
