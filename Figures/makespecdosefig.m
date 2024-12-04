b702717eb5f9b369e79c752deea5e184a731b8cb
Francisco J. Flores <fjflores@mit.edu>
2024-12-03 23:11:41 -0500

    use newest tidydata.

--- Figures/makespecdosefig.m
+++ Figures/makespecdosefig.m
@@ -11,7 +11,7 @@ gap = [ 0.005 0.01 ];
 margH = [ 0.1 0.05 ];
 margV = [0.1 0.1];
 opts = { gap, margH, margV };
-yLims = [ 0 50 ];
+yLims = [ 0 40 ];
 
 figure( 'WindowState', 'maximized' )
 colormap magma
@@ -22,22 +22,20 @@ for idxExp = 1 : nExps
     metDat = getmetadata( thisExp );
     resDir = fullfile( root, "Results", metDat.subject );
     f2load = "TidyData.mat";
-    load( fullfile( resDir, f2load ), "spec", "info" );
+    load( fullfile( resDir, f2load ), "spec", "notes" );
     % tsTab = readtable( fullfile( resDir, csvFileSpec ) );
-    tabExpIdx = find( [ info.expId ] == thisExp );
+    tabExpIdx = find( [ notes.expId ] == thisExp );
     S = spec( tabExpIdx ).SL;
-    t = ( spec( tabExpIdx ).t - ...
-        ( spec( tabExpIdx ).t( 1 ) + 600 ) ) / 60;
+    t = spec( tabExpIdx ).t ./ 60;
     f = spec( tabExpIdx ).f;
     
-    hAx( idxExp ) = subtightplot( nExps, 1, idxExp,...
-        opts{ : } );
+    hAx( idxExp ) = subtightplot( nExps, 1, idxExp, opts{ : } );
     imagesc( t, f, pow2db( S' ) )
     axis xy
     clim( [ -35 -5 ])
     box off
     xLims = get( gca, 'xlim' );
-    yLims = get( gca, 'ylim' );
+    ylim( yLims )
     posX = xLims( 1 ) + 1;
     posY = yLims( 2 ) - 5;
     tit = sprintf( '%s', metDat.subject );
@@ -55,7 +53,8 @@ set( hAx,...
     'XTickLabel', [],...
     'YTick',  0 : 10 : 50  )
 ffcbar( gcf, hAx( end ), "Power (dB)" );
-hAx( 1 ).Title.String = sprintf( "Dose: %u %cg/kg", dose, 956 );
+hAx( 1 ).Title.String = sprintf(...
+    "Spectrograms at %u %cg/kg in each mouse", dose, 956 );
 
 set( hAx( end ),...
     "XTick", [ -10 : 10 : 60 ],...
--- Figures/plotavedosespec.m
+++ Figures/plotavedosespec.m
@@ -6,7 +6,7 @@ masterTab = readtable( fullfile( root, "Results", csvFileMaster ) );
 
 gap = [ 0.005 0.01 ];
 margH = [ 0.1 0.05 ];
-margV = [0.1 0.1];
+margV = [ 0.1 0.1 ];
 opts = { gap, margH, margV };
 
 yLims = [ 0 40 ];
@@ -17,7 +17,7 @@ nDoses = length( doses );
 for doseIdx = 1 : nDoses
     thisDose = doses( doseIdx );
     expListIdx = masterTab.analyze == 1 &...
-        masterTab.dex_dose_ugperkg == thisDose;
+        masterTab.drug_dose == thisDose;
     expList = masterTab.exp_id( expListIdx );
     fprintf( "Processing dose %u %cg\\kg...\n", thisDose, 965 )
     
@@ -26,16 +26,15 @@ for doseIdx = 1 : nDoses
         thisExp = expList( idxExp );
         metDat = getmetadata( thisExp );
         resDir = fullfile( root, "Results", metDat.subject );
-        f2load = "ExampleFigData.mat";
-        load( fullfile( resDir, f2load ), "spec", "info" );
-        tabExpIdx = find( [ info.expId ] == thisExp );
-        S( :, :, idxExp ) = spec( tabExpIdx ).L;
+        f2load = "TidyData.mat";
+        load( fullfile( resDir, f2load ), "spec", "notes" );
+        tabExpIdx = find( [ notes.expId ] == thisExp );
+        S( :, :, idxExp ) = spec( tabExpIdx ).SL;
         Sdose( :, : ) = median( S, 3 );
 
         if idxExp == 1
-            t = ( spec( tabExpIdx ).t2plot - ...
-            ( spec( tabExpIdx ).t2plot( 1 ) + 600 ) ) / 60;
-            f = spec( tabExpIdx ).f2plot;
+            t = spec( tabExpIdx ).t / 60;
+            f = spec( tabExpIdx ).f;
 
         end
 
@@ -47,15 +46,16 @@ for doseIdx = 1 : nDoses
     imagesc( t, f, pow2db( Sdose' ) )
     axis xy
     box off
-    clim( [ -35 0 ] )
+    clim( [ -35 -5 ] )
+    ylim( yLims )
     ylabel( 'Freq. (Hz)' )
     xLims = get( gca, 'xlim' );
-    yLims = get( gca, 'ylim' );
     posX = xLims( 1 ) + 1;
     posY = yLims( 2 ) - 5;
 
     if thisDose == 0
         tit = "Saline";
+        title( "Average spectrograms per dose")
 
     else
         tit = sprintf( "Dose: %u %cg/kg", thisDose, 956 );
@@ -80,8 +80,8 @@ set( hAx,...
 ffcbar( gcf, hAx( end ), "Power (dB)" );
 
 set( hAx( end ),...
-    "XTick", [ -10 : 5 : 60 ],...
-    "XTickLabel", [ -10 : 5 : 60 ] )
+    "XTick", [ -9.8 0 : 10 : 50 59.8 ],...
+    "XTickLabel", [ -10 : 10 : 60 ] )
 xlabel( hAx( end - 1 : end ), "time (min)" );
 set( hAx, 'FontSize', 12, 'TickDir', 'out' )
 % set( gcf, "Units", "normalized", "Position", [ 0.30 0.31 0.37 0.47 ] )
