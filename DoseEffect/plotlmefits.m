b3bbc16d136fa23d8499d3ea7a896eea83d68fce
Francisco J. Flores <fjflores@mit.edu>
2025-03-03 15:53:58 -0500

    Add plotting proper confidence intervals.

--- DoseEffect/plotlmefits.m
+++ DoseEffect/plotlmefits.m
@@ -5,7 +5,7 @@ function plotlmefits( mdls, feat2plot, varargin )
 ciFlag = false;
 col2plot = 'k';
 tmp = magma;
-cMap = tmp ( 15 : 20 : end, : );
+cMap = tmp ( 20 : 15 : end, : );
 
 % Parse  name-value pairs
 names = lower( varargin( 1 : 2 : end ) );
@@ -14,16 +14,16 @@ for k = 1 : numel( names )
     switch lower( names{ k } )
         case "color"
             col2plot = values{ k };
-            
+
         case "plotci"
             ciFlag = values{ k };
-            
+
         otherwise
             error( '''%s'' is not a valid Name for Name, Value pairs.',...
                 names{ k } )
-            
+
     end
-    
+
 end
 
 % Generate predictor values for plotting
@@ -52,27 +52,42 @@ for mdlIdx = 1 : length( mdls )
     % mockTab = table(firstColumn, secondColumn, 'VariableNames', { 'mouseId', 'dose' } );
     % predVals = predict( mdls( 1 ).rmsEmg, mockTab )
 
+    hold on;
     if ciFlag
         % Calculate standard errors of the fitted values
-        standardErrors = std( resi ) * sqrt( 1 + ( 1 / length( resi ) ) ); % Adjust as needed
+        % standardErrors = std( resi ) * sqrt( 1 + ( 1 / length( resi ) ) ); % Adjust as needed
+        %
+        % % Calculate studentized confidence intervals
+        % tValue = tinv( 0.975, mdls( mdlIdx ).( feat2plot ).DFE ); % 95% confidence interval
+        % lowerCI = fittedVals - tValue * standardErrors;
+        % upperCI = fittedVals + tValue * standardErrors;
+        mse = mean( (...
+            mdls( mdlIdx ).( feat2plot ).Variables{ :, feat2plot } - ...
+            predict( mdls( mdlIdx ).( feat2plot ), 'Conditional', false ) ) .^ 2 );
+        dfe = mdls( mdlIdx ).( feat2plot ).DFE;
+        [ upperCI, lowerCI ] = getcis( dose, fittedVals, mse, dfe );
 
-        % Calculate studentized confidence intervals
-        tValue = tinv( 0.975, mdls( mdlIdx ).( feat2plot ).DFE ); % 95% confidence interval
-        lowerCI = fittedVals - tValue * standardErrors;
-        upperCI = fittedVals + tValue * standardErrors;
+        if length( col2plot ) == 1
+            fill( ...
+                [ dose; flipud( dose ) ], ...
+                [ lowerCI; flipud( upperCI ) ],...
+                col2plot, ...
+                'FaceAlpha', 0.2, ...
+                'EdgeColor', 'none' );
+
+        else
+            fill( ...
+                [ dose; flipud( dose ) ], ...
+                [ lowerCI; flipud( upperCI ) ],...
+                cMap( mdlIdx, : ), ...
+                'FaceAlpha', 0.2, ...
+                'EdgeColor', 'none' );
+
+        end
 
     end
 
-    % Plot the original data and fitted values with confidence intervals
-    % figure;
-    hold on;
-    
-    if ciFlag
-        fill( [ dose; flipud( dose ) ] , [ lowerCI; flipud( upperCI ) ],...
-            colors( mdlIdx, : ), 'FaceAlpha', 0.2, 'EdgeColor', 'none');
-
-    end
-    
+    % Plot the original data and fitted values with confidence intervals.
     if length( col2plot ) == 1
         plot( dose, fittedVals,...
             'LineWidth', 2, 'Color', col2plot );
@@ -84,3 +99,18 @@ for mdlIdx = 1 : length( mdls )
     end
 
 end
+
+
+% Helper functions
+function [ upper, lower ] = getcis( dose, fitted, mse, dfe )
+
+X = [ ones( size( dose ) ) dose ];
+[ ~, R ] = qr( X, 0 );
+% E = [ ones( size( x ) ), x ] / R;
+E = X / R;
+alpha = 0.05;
+dy = -tinv( alpha / 2, dfe ) * sqrt( sum( E .^ 2, 2 ) * mse );
+
+% Compute fitted line and bounds on this grid
+upper = fitted + dy;
+lower = fitted - dy;
