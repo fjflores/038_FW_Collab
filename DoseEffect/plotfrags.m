c148767e0cb60a85454dfd30a9a175874ea5abaf
Isabella Dalla Betta <idallabe@mit.edu>
2025-03-31 19:41:06 -0400

    alternative plot for comparing time in high delta

--- DoseEffect/plotfrags.m
+++ DoseEffect/plotfrags.m
@@ -1,5 +1,7 @@
 %% plot fragments
-ccc
+% ccc
+clear all
+clc
 frags = finddeltafrags( "dex", "delta", "total" );
 
 % Process frags per dose
@@ -8,8 +10,8 @@ for i = 1 : length( frags )
     rats{ :, i } = frags( i ).lowDur ./ frags( i ).highDur;
     doses( i ) = frags( i ).dose;
     plot( doses( i ), rats{ :, i }, '.b',...
-        "MarkerSize", 20,...
-        "MarkerAlpha", 0.6 )
+        "MarkerSize", 20 )%,...
+        % "MarkerAlpha", 0.6 )
     hold on
 
 end
@@ -18,8 +20,51 @@ xlim( [ -10 160 ] )
 ylim( [ -1 10 ] )
 hAx = gca;
 set( hAx, "XTick", doses )
-xlabel( fprintf( "Dex dose (%cg/kg)", 965 ) )
-ylabel( "Ratio (\delta_L\\\delta_H )" )
+xlabel( sprintf( "Dex dose (%cg/kg)", 956 ) )
+ylabel( "Ratio (\delta_L/\delta_H )" )
+
+
+% 
+% figure
+% scatter( doses, rats )
+
+
+
+%% plot fragments option B
+ccc
+frags = finddeltafrags( "dex", "delta", "total" );
+
+% Process frags per dose
+figure
+hold on
+tmpDoses = [ 0 1 2 3 4 5 ];
+offset = 0.2;
+loCol = [ 217,95,2 ] / 255;
+hiCol = [ 27,158,119 ] / 255;
+for i = 1 : length( frags )
+    
+    realDoses( i ) = frags( i ).dose;
+    % doses( i ) = tmpDoses( i );
+doses( i ) = frags( i ).dose;
+    % scatter( doses( i ) - offset, frags( i ).lowDur / 60,...
+    %     'filled', 'MarkerFaceColor', loCol )
+    scatter( doses( i ) + offset, frags( i ).highDur / 60,...
+        'filled', 'MarkerFaceColor', hiCol )
+    % bar( doses( i ) - offset, mean( frags( i ).lowDur / 60 ),...
+    %     2 * offset, 'FaceColor', loCol, 'FaceAlpha', 0.5, 'EdgeColor', loCol )
+    % bar( doses( i ) + offset, mean( frags( i ).highDur / 60 ),...
+    %     2 * offset, 'FaceColor', hiCol, 'FaceAlpha', 0.5, 'EdgeColor', hiCol )
+
+end
+box off
+% xlim( [ -0.9 5.9 ] )
+xlim( [ -10 160 ] )
+ylim( [ 0 60 ] )
+hAx = gca;
+set( hAx, "XTick", doses )
+set( hAx, "XTickLabels", realDoses )
+xlabel( sprintf( "Dex dose (%cg/kg)", 956 ) )
+ylabel( "Time spent in \delta_H (min)" )
 
 
 % 
